a87b9b428ca013a900d128b0de409559fa433b7a
vagrant <vagrant@homestead>
2018-05-25 07:12:40 +0000

    Basic user profile editing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
+                @include('layouts._message')
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
